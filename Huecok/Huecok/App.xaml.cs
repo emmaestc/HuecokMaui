c353fb12132aa207e23e83bd5e883a9c4bbd7f0e
emmaestc <emmanuelestrada898@gmail.com>
2023-05-31 15:32:22 -0500

    Initial comit

---
+++ Huecok/Huecok/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Huecok;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Huecok/Huecok/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Huecok;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
